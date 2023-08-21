58e9dc2bc536c5175c52d64b0ae07e090e90b7cd
liestreadt <Nik_vladislav@outlook.com>
2023-08-21 19:57:15 +0300

    asd

--- insertionSort.js
+++ insertionSort.js
@@ -1,23 +1,20 @@
-const asd = [-2, -15, 0, 1000, 1, 4, 2, 10, 5]
+const asd = [-10, -9, -5, 0, 12, 5,4,3,2,1]
 
 const insertionSort = (arr) => {
     if (!arr.length) return [];
-
     const result = [arr[0]];
-
     for (let i = 1; i < arr.length; i += 1) {
         const item = arr[i];
         let index = i;
         for (let j = 0; j < result.length; j += 1) {
             if (item < result[j]) {
                 index = j;
+                break
             }
         }
         result.splice(index, 0 , item)
     }
-
     return result;
-
 }
 
 console.log(insertionSort(asd));
